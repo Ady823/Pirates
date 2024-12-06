b96e3c7e90949bb78c1401bd113d9cf4456ab087
Ady823 <aduval@brooksschool.org>
2024-12-06 14:55:52 +0000

    deck pt 2

--- src/main/PiratePairs.java
+++ src/main/PiratePairs.java
@@ -1,5 +1,10 @@
+import java.util.Arrays; 
 public class PiratePairs{
     public static void main(String[] args){
         System.out.println("Hello World!");
+        Player player1 = new Player();
+        Player player2 = new Player();
+        Player player3 = new Player();
+        Deck deck1 = new Deck();
     }
-}
+} 
