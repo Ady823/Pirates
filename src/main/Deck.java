78a8cafe3e5b57202a5b52d93f5ac874ba3c0b35
Ady823 <aduval@brooksschool.org>
2024-12-07 02:20:50 +0000

    shuffling

--- src/main/Deck.java
+++ src/main/Deck.java
@@ -17,4 +17,16 @@ public class Deck{
     public int[] getArray(){
         return deckArray;
     }
+    public int[] shuffleDeck(){
+        int[] shuffledDeck = new int[55];
+
+        for (int i = 0; i < deckArray.length; i++) {
+
+            if(deckArray[i] != -1) {
+                shuffledDeck[(int)(Math.random() * 56)];
+                deckArray[i] = -1;
+            }
+            
+        }
+    }
 }
