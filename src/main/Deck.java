843fda01246d2aa9d7b552e6e5bc57d5c62e3dbd
Ady823 <aduval@brooksschool.org>
2024-12-12 15:41:54 +0000

    formatting

--- src/main/Deck.java
+++ src/main/Deck.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
-public class Deck{ 
+
+public class Deck {
     int[] deckArray = new int[55];
     int productFactor = 56;
     int[] discardArray = new int[55];
@@ -7,6 +8,7 @@ public class Deck{
     public int[] discard() {
         return discardArray;
     }
+
     public void addToDiscard(int[] deck) {
         for (int i = 0; i < discardArray.length; i++) {
             if (i != 0) {
@@ -14,40 +16,42 @@ public class Deck{
             }
         }
 
-        discardArray = deck; 
+        discardArray = deck;
     }
-    public Deck(){
-        int deckVal = 1; 
 
-        for(int i = 0; i < deckArray.length;){
-            for(int j = deckVal; j > 0; j--){
+    public Deck() {
+        int deckVal = 1;
+
+        for (int i = 0; i < deckArray.length;) {
+            for (int j = deckVal; j > 0; j--) {
                 deckArray[i] = deckVal;
                 i++;
-            }  
+            }
             deckVal++;
         }
     }
-    public int[] getArray(){
+
+    public int[] getArray() {
         return deckArray;
     }
 
-    public int[] shuffledDeck(){
-        for(int i = deckArray.length - 1; i > 0; i--){
-        int randomIndex = (int)(Math.random() * deckArray.length);
-        int temp = deckArray[randomIndex];
-        deckArray[randomIndex] = deckArray[i]; 
-        deckArray[i] = temp; 
+    public int[] shuffledDeck() {
+        for (int i = deckArray.length - 1; i > 0; i--) {
+            int randomIndex = (int) (Math.random() * deckArray.length);
+            int temp = deckArray[randomIndex];
+            deckArray[randomIndex] = deckArray[i];
+            deckArray[i] = temp;
         }
         return deckArray;
     }
-    public int getNextCard(){
+
+    public int getNextCard() {
         int lastCard = deckArray[deckArray.length - 1];
         int[] tempArray = new int[deckArray.length - 1];
-        for (int i = 0; i < tempArray.length; i++){
-           tempArray[i] = deckArray[i];
+        for (int i = 0; i < tempArray.length; i++) {
+            tempArray[i] = deckArray[i];
         }
         deckArray = tempArray;
         return lastCard;
     }
 }
-
--- src/main/Player.java
+++ src/main/Player.java
@@ -1,4 +1,4 @@
-public class Player{
+public class Player {
     private static int playerCount = 0;
     private int cardCount;
     private int[] cards;
@@ -11,77 +11,86 @@ public class Player{
         cardCount = 0;
         points = 0;
     }
+
     public static int playerCount() {
         return playerCount;
     }
+
     public int cardCount() {
         return cardCount;
     }
+
     public void setCard(int cardNum) {
         cards[cardCount] = cardNum;
     }
-    public int[] showCards(){
-        return cards; 
+
+    public int[] showCards() {
+        return cards;
     }
 
-    // private void playerTurn(){ 
-    //     if (cardCount == 0) {
-    //         takeCard();
-    //     } 
+    // private void playerTurn(){
+    // if (cardCount == 0) {
+    // takeCard();
     // }
-    public void takeCard(int card){ 
+    // }
+    public void takeCard(int card) {
         int[] tempCards = new int[cards.length + 1];
-        for(int i = 0; i < cards.length; i++) {
-            tempCards[i] = cards[i]; 
+        for (int i = 0; i < cards.length; i++) {
+            tempCards[i] = cards[i];
         }
         tempCards[tempCards.length - 1] = card;
         cards = tempCards;
-    } 
-    public boolean has(int card){
+    }
+
+    public boolean has(int card) {
         boolean has = false;
-        for (int i = 0; i < cards.length; i++){
+        for (int i = 0; i < cards.length; i++) {
             if (card == cards[i]) {
                 has = true;
-            }   
+            }
         }
         return has;
     }
-    private int topCard(int[] deck){
+
+    private int topCard(int[] deck) {
         int topCard = 0;
-        while(topCard == 0) {
-            for(int i = deck.length; i >= 0; i++){
+        while (topCard == 0) {
+            for (int i = deck.length; i >= 0; i++) {
                 if (deck[i] != 0) {
                     topCard = deck[i];
                 }
             }
-        } 
+        }
         return topCard;
-    } 
+    }
 
-    public int points(){
+    public int points() {
         return points;
     }
-    public void addPoints(int card){
+
+    public void addPoints(int card) {
         points += card;
     }
 
     public boolean loss(Player p) {
         boolean loss = false;
-        if(p.points() >= 60 / playerCount + 1) {
+        if (p.points() >= 60 / playerCount + 1) {
             loss = true;
         }
         return loss;
     }
-  /*   public void pair(Deck deck){
-        while (gameEnd == false) {
-            int card = deck.getNextCard();
-            if (Player.has(card)) {
-                Player.addPoints(card);
-                System.out.println("player 1 has " + Player.points());
-                gameEnd = true;
-             } else {
-                 player1.takeCard(card);
-             }
-         } 
-    } */
+    /*
+     * public void pair(Deck deck){
+     * while (gameEnd == false) {
+     * int card = deck.getNextCard();
+     * if (Player.has(card)) {
+     * Player.addPoints(card);
+     * System.out.println("player 1 has " + Player.points());
+     * gameEnd = true;
+     * } else {
+     * player1.takeCard(card);
+     * }
+     * }
+     * }
+     */
 }
